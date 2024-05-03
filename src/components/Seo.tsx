912f5801ddb90a1839d8cced8a0c95a55b219009
sarthakskumar <sarthakskumar7@gmail.com>
2024-05-03 20:45:32 +0530

    config: update favicon

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -141,7 +141,61 @@ const favicons: Array<Favicons> = [
     rel: 'icon',
     type: 'image/png',
     sizes: '192x192',
-    href: '/favicon/android-icon-192x192.png',
+    href: '/favicon/android-chrome-192x192.png',
+  },
+  {
+    rel: 'icon',
+    href: "/android-chrome-36x36.png",
+    sizes: "36x36",
+    type: "image/png"
+  },
+  {
+    rel: 'icon',
+    href: "/android-chrome-48x48.png",
+    sizes: "48x48",
+    type: "image/png"
+  },
+  {
+    rel: 'icon',
+    href: "/android-chrome-72x72.png",
+    sizes: "72x72",
+    type: "image/png"
+  },
+  {
+    rel: 'icon',
+    href: "/android-chrome-96x96.png",
+    sizes: "96x96",
+    type: "image/png"
+  },
+  {
+    rel: 'icon',
+    href: "/android-chrome-144x144.png",
+    sizes: "144x144",
+    type: "image/png"
+  },
+  {
+    rel: 'icon',
+    href: "/android-chrome-192x192.png",
+    sizes: "192x192",
+    type: "image/png"
+  },
+  {
+    rel: 'icon',
+    href: "/android-chrome-256x256.png",
+    sizes: "256x256",
+    type: "image/png"
+  },
+  {
+    rel: 'icon',
+    href: "/android-chrome-384x384.png",
+    sizes: "384x384",
+    type: "image/png"
+  },
+  {
+    rel: 'icon',
+    href: "/android-chrome-512x512.png",
+    sizes: "512x512",
+    type: "image/png"
   },
   {
     rel: 'icon',
@@ -153,13 +207,29 @@ const favicons: Array<Favicons> = [
     rel: 'icon',
     type: 'image/png',
     sizes: '96x96',
-    href: '/favicon/favicon-96x96.png',
+    href: '/favicon/android-chrome-96x96.png',
+  },
+  {
+    rel: 'sitemap',
+    type: 'application/xml',
+    href: '/sitemap.xml'
   },
   {
     rel: 'icon',
     type: 'image/png',
     sizes: '16x16',
     href: '/favicon/favicon-16x16.png',
+  },
+  {
+    rel: 'icon',
+    type: 'image/png',
+    sizes: "144x144",
+    href: '/favicon/mstile-144x144.png',
+  }, {
+    rel: 'icon',
+    type: 'image/png',
+    sizes: "150x150",
+    href: '/favicon/mstile-150x150.png',
   },
   {
     rel: 'manifest',
